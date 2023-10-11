d2f87f84942a74b60818e7de740bb54757e74741
hrisz <harissaefuloh@gmail.com>
2023-10-11 14:54:29 +0700

    input tugas & praktikum

---
+++ Pertemuan03/Praktikum/P3_1_714220061/P3_1_714220061/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P3_1_714220061
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.Clear();
+
+                Console.Write("MENENTUKAN INDEKS PRESTASI MAHASISWA\n-------------------------------------");
+                Console.Write("\nMasukkan Nama Mahasiswa: ");
+
+                String nama = Console.ReadLine();
+
+                Console.Write("Masukkan Nilai: ");
+
+                int nilai = Convert.ToInt16(Console.ReadLine());
+
+                String[] grade = { "A", "B", "C", "D", };
+                if (nilai >= 85)
+                {
+                    Console.WriteLine("Indeks nilai {0} adalah {1}", nama, grade[0]);
+                }
+                else if (nilai >= 70 && nilai < 85)
+                {
+                    Console.WriteLine("Indeks nilai {0} adalah {1}", nama, grade[1]);
+                }
+                else if (nilai >= 60 && nilai < 70)
+                {
+                    Console.WriteLine("Indeks nilai {0} adalah {1}", nama, grade[2]);
+                }
+                else
+                {
+                    Console.WriteLine("Indeks nilai {0} adalah {1}", nama, grade[3]);
+                }
+
+                Console.Write("\nMasukkan indeks yang ditampilkan: ");
+                char indeks = Convert.ToChar(Console.ReadLine());
+                Console.Write("Indeks prestasi {0} adalah ", nama);
+
+                prestasi(indeks);
+                Console.Write("\nIngin mengulang kembali? (Y/T): ");
+            }
+            while (Console.ReadLine() == "Y");
+        }
+
+        private static void prestasi(char indeks)
+        {
+            switch (indeks)
+            {
+                case 'A':
+                    Console.WriteLine("Sangat Baik");
+                    break;
+                case 'B':
+                    Console.WriteLine("Baik");
+                    break;
+                case 'C':
+                    Console.WriteLine("Cukup");
+                    break;
+                case 'D':
+                    Console.WriteLine("Buruk");
+                    break;
+                default:
+                    Console.WriteLine("Invalid indeks prestasi");
+                    break;
+            }
+        }
+    }
+}
---
+++ Pertemuan03/Praktikum/P3_3_714220061/P3_3_714220061/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P3_3_714220061
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("MENU PERSEGI PANJANG");
+                Console.WriteLine("1. Hitung Luas");
+                Console.WriteLine("2. Hitung Keliling");
+
+                Console.Write("Menu Pilihan: ");
+                int input_menu = Convert.ToInt32(Console.ReadLine());
+
+                if (input_menu == 1)
+                {
+                    Console.WriteLine("MENGHITUNG LUAS PERSEGI PANJANG");
+                    Console.Write("Masukkan Panjang: ");
+                    int panjang = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Masukkan Panjang: ");
+                    int lebar = Convert.ToInt32(Console.ReadLine());
+
+                    int luas = panjang * lebar;
+                    Console.WriteLine("Luas Persegi Panjang = {0}", luas);
+                }
+
+                else if (input_menu == 2)
+                {
+                    Console.WriteLine("MENGHITUNG KELILING PERSEGI PANJANG");
+                    Console.Write("Masukkan Panjang: ");
+                    int panjang = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Masukkan Panjang: ");
+                    int lebar = Convert.ToInt32(Console.ReadLine());
+
+                    int keliling = 2 * (panjang + lebar);
+                    Console.WriteLine("Luas Persegi Panjang = {0}", keliling);
+                }
+
+                else
+                {
+                    Console.WriteLine("Menu tidak tersedia");
+                }
+                Console.Write("\nIngin mengulang kembali? (Y/T)");
+            }
+            while (Console.ReadLine() == "Y");
+        }
+    }
+}
