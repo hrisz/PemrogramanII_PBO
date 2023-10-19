bf345cb7d8d1497a0e9c5edba0951edd68f07433
hrisz <harissaefuloh@gmail.com>
2023-10-19 10:56:05 +0700

    Praktikum ke-4

---
+++ Pertemuan04/Praktikum/P4_1_714220061/P4_1_714220061/P4_1_714220061/Book_714220061.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P4_1_714220061
+{
+    public class Book_714220061: Product_714220061
+    {
+        protected string pageCount;
+
+        public Book_714220061(string type, string title, string pagecount): base(type, title)
+        {
+            this.pageCount = pagecount;
+        }
+
+        public string PageCount
+        { 
+            get 
+            { 
+                return pageCount; 
+            } 
+            set 
+            { 
+                this.pageCount = value; 
+            }
+        }
+    }
+}
