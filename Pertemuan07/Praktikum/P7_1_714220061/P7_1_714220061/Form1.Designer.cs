feefd0cf049136aa80a257ecbf173b210e3b3e98
hrisz <harissaefuloh@gmail.com>
2023-11-08 23:03:56 +0700

    P7

---
+++ Pertemuan07/Praktikum/P7_1_714220061/P7_1_714220061/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace P7_1_714220061
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.formName = new System.Windows.Forms.TextBox();
+            this.formStudy = new System.Windows.Forms.TextBox();
+            this.formClass = new System.Windows.Forms.TextBox();
+            this.buttonCheck = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbKuliah = new System.Windows.Forms.CheckBox();
+            this.cbTravel = new System.Windows.Forms.CheckBox();
+            this.cbTuru = new System.Windows.Forms.CheckBox();
+            this.radMonday = new System.Windows.Forms.RadioButton();
+            this.radSunday = new System.Windows.Forms.RadioButton();
+            this.buttonPrint = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nama";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Prodi";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Kelas";
+            // 
+            // formName
+            // 
+            this.formName.Location = new System.Drawing.Point(139, 43);
+            this.formName.Name = "formName";
+            this.formName.Size = new System.Drawing.Size(273, 22);
+            this.formName.TabIndex = 3;
+            // 
+            // formStudy
+            // 
+            this.formStudy.Location = new System.Drawing.Point(139, 90);
+            this.formStudy.Name = "formStudy";
+            this.formStudy.Size = new System.Drawing.Size(273, 22);
+            this.formStudy.TabIndex = 4;
+            // 
+            // formClass
+            // 
+            this.formClass.Location = new System.Drawing.Point(139, 136);
+            this.formClass.Name = "formClass";
+            this.formClass.Size = new System.Drawing.Size(273, 22);
+            this.formClass.TabIndex = 5;
+            // 
+            // buttonCheck
+            // 
+            this.buttonCheck.Location = new System.Drawing.Point(139, 194);
+            this.buttonCheck.Name = "buttonCheck";
+            this.buttonCheck.Size = new System.Drawing.Size(107, 33);
+            this.buttonCheck.TabIndex = 6;
+            this.buttonCheck.Text = "Cek";
+            this.buttonCheck.UseVisualStyleBackColor = true;
+            this.buttonCheck.Click += new System.EventHandler(this.buttonCheck_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(264, 194);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 33);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Tutup Form";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(166, 299);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(151, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "LIST KEGIATAN";
+            // 
+            // cbKuliah
+            // 
+            this.cbKuliah.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbKuliah.AutoSize = true;
+            this.cbKuliah.Location = new System.Drawing.Point(53, 411);
+            this.cbKuliah.Name = "cbKuliah";
+            this.cbKuliah.Size = new System.Drawing.Size(65, 20);
+            this.cbKuliah.TabIndex = 9;
+            this.cbKuliah.Text = "Kuliah";
+            this.cbKuliah.UseVisualStyleBackColor = true;
+            // 
+            // cbTravel
+            // 
+            this.cbTravel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbTravel.AutoSize = true;
+            this.cbTravel.Location = new System.Drawing.Point(53, 437);
+            this.cbTravel.Name = "cbTravel";
+            this.cbTravel.Size = new System.Drawing.Size(89, 20);
+            this.cbTravel.TabIndex = 10;
+            this.cbTravel.Text = "Travelling";
+            this.cbTravel.UseVisualStyleBackColor = true;
+            // 
+            // cbTuru
+            // 
+            this.cbTuru.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbTuru.AutoSize = true;
+            this.cbTuru.Location = new System.Drawing.Point(53, 463);
+            this.cbTuru.Name = "cbTuru";
+            this.cbTuru.Size = new System.Drawing.Size(60, 20);
+            this.cbTuru.TabIndex = 11;
+            this.cbTuru.Text = "Tidur";
+            this.cbTuru.UseVisualStyleBackColor = true;
+            // 
+            // radMonday
+            // 
+            this.radMonday.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.radMonday.AutoSize = true;
+            this.radMonday.Location = new System.Drawing.Point(53, 342);
+            this.radMonday.Name = "radMonday";
+            this.radMonday.Size = new System.Drawing.Size(62, 20);
+            this.radMonday.TabIndex = 12;
+            this.radMonday.TabStop = true;
+            this.radMonday.Text = "Senin";
+            this.radMonday.UseVisualStyleBackColor = true;
+            this.radMonday.CheckedChanged += new System.EventHandler(this.radMonday_CheckedChanged);
+            // 
+            // radSunday
+            // 
+            this.radSunday.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.radSunday.AutoSize = true;
+            this.radSunday.Location = new System.Drawing.Point(53, 368);
+            this.radSunday.Name = "radSunday";
+            this.radSunday.Size = new System.Drawing.Size(72, 20);
+            this.radSunday.TabIndex = 13;
+            this.radSunday.TabStop = true;
+            this.radSunday.Text = "Minggu";
+            this.radSunday.UseVisualStyleBackColor = true;
+            this.radSunday.CheckedChanged += new System.EventHandler(this.radSunday_CheckedChanged);
+            // 
+            // buttonPrint
+            // 
+            this.buttonPrint.Location = new System.Drawing.Point(139, 499);
+            this.buttonPrint.Name = "buttonPrint";
+            this.buttonPrint.Size = new System.Drawing.Size(107, 35);
+            this.buttonPrint.TabIndex = 14;
+            this.buttonPrint.Text = "Print";
+            this.buttonPrint.UseVisualStyleBackColor = true;
+            this.buttonPrint.Click += new System.EventHandler(this.buttonPrint_Click);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(264, 499);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(107, 35);
+            this.buttonReset.TabIndex = 15;
+            this.buttonReset.Text = "Reset Form";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 583);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonPrint);
+            this.Controls.Add(this.radSunday);
+            this.Controls.Add(this.radMonday);
+            this.Controls.Add(this.cbTuru);
+            this.Controls.Add(this.cbTravel);
+            this.Controls.Add(this.cbKuliah);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.buttonCheck);
+            this.Controls.Add(this.formClass);
+            this.Controls.Add(this.formStudy);
+            this.Controls.Add(this.formName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox formName;
+        private System.Windows.Forms.TextBox formStudy;
+        private System.Windows.Forms.TextBox formClass;
+        private System.Windows.Forms.Button buttonCheck;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox cbKuliah;
+        private System.Windows.Forms.CheckBox cbTravel;
+        private System.Windows.Forms.CheckBox cbTuru;
+        private System.Windows.Forms.RadioButton radMonday;
+        private System.Windows.Forms.RadioButton radSunday;
+        private System.Windows.Forms.Button buttonPrint;
+        private System.Windows.Forms.Button buttonReset;
+    }
+}
+
---
+++ Pertemuan07/Praktikum/P7_1_714220061/P7_1_714220061/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P7_1_714220061
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Size = new Size(397, 242);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonCheck_Click(object sender, EventArgs e)
+        {
+            string errorMessage = "";
+            
+            if (string.IsNullOrWhiteSpace(formName.Text))
+            {
+                errorMessage += "Nama belum diisi\n";
+            }
+            if (string.IsNullOrWhiteSpace(formStudy.Text))
+            {
+                errorMessage += "Prodi belum diisi\n";
+            }
+            else if (!Regex.IsMatch(formStudy.Text, @"^[A-Za-z0-9]+-[A-Za-z0-9]+$"))
+            {
+                errorMessage += "Prodi harus berformat [Strata]-[Prodi]\n";
+            }
+            if (string.IsNullOrWhiteSpace(formClass.Text))
+            {
+                errorMessage += "Kelas belum diisi\n";
+            }
+
+            if(string.IsNullOrEmpty(errorMessage))
+            {
+                MessageBox.Show(
+                    "Lengkap","Informasi Data Submit",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information
+                    );
+
+                this.Size = new Size(397, 492);
+            }
+            else
+            {
+                MessageBox.Show(
+                    errorMessage, "Informasi Data Submit",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning
+                    );
+            }
+
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            this.Size = new Size(397, 242);
+
+            foreach (Control control in Controls)
+            {
+                if (control is TextBox) ((TextBox)control).Text = string.Empty;
+                else if (control is RadioButton) ((RadioButton)control).Checked = false;
+                else if (control is CheckBox) ((CheckBox)control).Checked = false;
+                else if (control is ComboBox) ((ComboBox)control).SelectedIndex = -1;
+            }
+
+        }
+
+        private void radMonday_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radMonday.Checked)
+            {
+                cbKuliah.Enabled = true; cbKuliah.Checked = true;
+                cbTravel.Enabled = false; cbTravel.Checked = false;
+                cbTuru.Enabled = true; cbTuru.Checked = true;
+            }
+        }
+
+        private void radSunday_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radSunday.Checked)
+            {
+                cbKuliah.Enabled = false; cbKuliah.Checked = false;
+                cbTravel.Enabled = true; cbTravel.Checked = true;
+                cbTuru.Enabled = true; cbTuru.Checked = true;
+            }
+        }
+
+        private void buttonPrint_Click(object sender, EventArgs e)
+        {
+            bool day = radMonday.Checked || radSunday.Checked;
+            bool activity = cbKuliah.Checked || cbTravel.Checked || cbTuru.Checked;
+
+            string days = string.Empty;
+            string activities = string.Empty;
+
+            if (radMonday.Checked)
+            {
+                days += "Senin";
+            }
+            else if (radSunday.Checked)
+            {
+                days += "Minggu";
+            }
+
+            if (cbKuliah.Checked)
+            {
+                activities += "Kuliah, ";
+            }
+            if (cbTravel.Checked)
+            {
+                activities += "Traveling, ";
+            }
+            if (cbTuru.Checked)
+            {
+                activities += "Tidur";
+            }
+            if (!string.IsNullOrEmpty(activities))
+            {
+                activities = activities.TrimEnd(' ', ',');
+            }
+
+            if (formName.Text != "" && formStudy.Text != "" && formClass.Text != "" && Regex.IsMatch(formStudy.Text, @"^[A-Za-z0-9]+-[A-Za-z0-9]+$") && day && activity)
+            {
+                MessageBox.Show("Nama: " + formName.Text + "\nProdi: " + formStudy.Text + "\nKelas: " + formClass.Text + "\nHari: " + days + "\nActivities: " + activities,"Hasil output", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Isi form terlebih dahulu dengan benar!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
